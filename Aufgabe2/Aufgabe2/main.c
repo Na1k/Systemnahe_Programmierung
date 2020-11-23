37450a60345d374a3bc29cdbd45a2b5889d230cc
Na1k <nick.kramer9926@gmail.com>
2020-11-24 00:38:08 +0100

    On branch aufgabe2_init

---
+++ Aufgabe2/Aufgabe2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aufgabe2.c
+ *
+ * Created: 24.11.2020 00:17:06
+ * Author : NickK
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
