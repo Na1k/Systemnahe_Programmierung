f8b99d99d7b2ae7e5cb9696eacf20ff0f5171596
Na1k <nick.kramer9926@gmail.com>
2021-01-09 13:18:13 +0100

    init Aufgabe7

---
+++ Aufgabe7/Aufgabe7/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aufgabe7.c
+ *
+ * Created: 09.01.2021 13:17:22
+ * Author : Gruppe3
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
